d51cb64a0d5f68a269d5c879b8fb1c07008dd213
Danny Van Stemp <danwardvs@adsgames.net>
2022-10-28 18:47:56 -0400

    stuff  code wireframe

--- a3/src/Cube.cpp
+++ a3/src/Cube.cpp
@@ -1,9 +1,22 @@
 #include "Cube.hpp"
+#include <GL/gl.h>
 
 Cube::Cube()
 {
-	// set set cordinate values for all vertices
-    // other faces
+vertex[0][0] = 0; vertex[0][1] = 0; vertex[0][2] = 0;
+vertex[1][0] = 0; vertex[1][1] = 1; vertex[1][2] = 0;
+vertex[2][0] = 1; vertex[2][1] = 1; vertex[2][2] = 0;
+vertex[3][0] = 1; vertex[3][1] = 0; vertex[3][2] = 0;
+vertex[4][0] = 0; vertex[4][1] = 0; vertex[4][2] = 1;
+vertex[5][0] = 0; vertex[5][1] = 1; vertex[5][2] = 1;
+vertex[6][0] = 1; vertex[6][1] = 1; vertex[6][2] = 1;
+vertex[7][0] = 1; vertex[7][1] = 0; vertex[7][2] = 1;
+face[0][0] = 0; face[0][1] = 1; face[0][2] = 2; face[0][3] = 3;
+face[1][0] = 7; face[1][1] = 6; face[1][2] = 5; face[1][3] = 4;
+face[2][0] = 0; face[2][1] = 4; face[2][2] = 5; face[2][3] = 1;
+face[3][0] = 2; face[3][1] = 1; face[3][2] = 5; face[3][3] = 6;
+face[4][0] = 3; face[4][1] = 2; face[4][2] = 6; face[4][3] = 7;
+face[5][0] = 0; face[5][1] = 3; face[5][2] = 7; face[5][3] = 4;
     r = 1.0;
     g = 1.0;
     b = 1.0;
@@ -11,15 +24,26 @@ Cube::Cube()
 
 void Cube::drawFace(int i)
 {
-// draw face i
+    glBegin(GL_LINE_LOOP);
+
+    glVertex3fv( &(vertex[face[i][0]][0]) );
+    glVertex3fv( &(vertex[face[i][1]][0]) );
+    glVertex3fv( &(vertex[face[i][2]][0]) );
+    glVertex3fv( &(vertex[face[i][3]][0]) );
+
+    glEnd();
+
 }
 
 void Cube::draw()
 {
     glPushMatrix();
     this->ctmMultiply();
- // set color
- // draw all faces
+    glScalef(s, s, s);
+	    for (int i = 0; i < 6; i++) {
+		    drawFace(i);
+	    }
+ 
     glPopMatrix();
 }
 
