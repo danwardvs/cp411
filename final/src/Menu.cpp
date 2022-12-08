fa3bf5348056459c7e6e68813f072b3f26c6a608
Bryan Gadd <x-dragola-x@live.ca>
2022-12-07 21:35:39 -0500

    Remove of extra menu stuff + update linux run

--- final/src/Menu.cpp
+++ final/src/Menu.cpp
@@ -20,17 +20,6 @@ extern int paused;
 
 
 void menu() {
-	GLint Cull_Menu = glutCreateMenu(cullMenu);
-	glutAddMenuEntry("No culling", 1);
-	glutAddMenuEntry("OpenGL back-face & depth buffer", 3);
-
-	GLint Shading_Menu = glutCreateMenu(shadeMenu);
-	glutAddMenuEntry("No shading", 1);
-	glutAddMenuEntry("My constant shading", 2);
-	glutAddMenuEntry("OpenGL flat shading", 3);
-	glutAddMenuEntry("OpenGL smooth shading", 4);
-	glutAddMenuEntry("Texture", 5);
-
 	GLint Level_Menu = glutCreateMenu(levelMenu);
 	glutAddMenuEntry("Level 1", 1);
 	glutAddMenuEntry("Level 2", 2);
@@ -42,8 +31,6 @@ void menu() {
 	glutAddMenuEntry("Reset", 1);
 	glutAddMenuEntry("Pause/Unpause", 3);
 	glutAddSubMenu("Level Select", Level_Menu);
-	glutAddSubMenu("Culling", Cull_Menu);
-	glutAddSubMenu("Shading", Shading_Menu);
 	glutAddMenuEntry("Quit", 2);
 }
 
@@ -64,50 +51,6 @@ void mainMenu(GLint option) {
 				paused=0;
 			else
 		 		paused=1;
-			break;
-	}
-	glutPostRedisplay();
-}
-
-void cullMenu(GLint option) {
-	switch (option){
-	  case 1:
-		cullMode = NONE;
-		glDisable(GL_CULL_FACE);
-		glDisable(GL_DEPTH_TEST);
-		break;
-	  case 3:
-		cullMode = GLCULL;
-		glCullFace(GL_BACK);
-		glEnable(GL_CULL_FACE);
-		glEnable(GL_DEPTH_TEST);
-		break;
-	}
-	glutPostRedisplay();
-}
-
-void shadeMenu(GLint option) {
-	glDisable(GL_TEXTURE_2D);
-	glDisable(GL_LIGHTING);
-	glDisable(GL_LIGHT0);
-	glDisable( GL_NORMALIZE);
-	glDisable(GL_COLOR_MATERIAL);
-	
-	switch (option) {
-		case 1:
-			renderMode = WIRE;
-			break;
-		case 2:
-			renderMode = CONSTANT;
-			break;
-		case 3:
-			renderMode = FLAT;
-			break;
-		case 4:
-			renderMode = SMOOTH;
-			break;
-		case 5:
-			renderMode = TEXTURE;
 			break;
 	}
 	glutPostRedisplay();
